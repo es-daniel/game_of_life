8db5ab25419a2c4b04c79b0ac94705b2ad51a355
DanielES <danielelizondo12@gmail.com>
2021-03-05 17:35:54 -0600

    Add a method to add random living cells

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -4,6 +4,19 @@ class GameOfLife
     @temp_grid = [[]]
   end
 
+  # This method creates initial pattern
+  def seed
+    5.times do |_|
+      row = %w[▢ ▢ ▢ ▢ ▢ ▢ ▢ ▢ ▢]
+      2.times { |_| rand_living_cell(row) }
+      @grid << row
+    end
+  end
+
+  def rand_living_cell(row)
+    row[rand(0..8)] = '▣'
+  end
+
   def draw
     @grid.each do |row|
       puts row.join(' ')
